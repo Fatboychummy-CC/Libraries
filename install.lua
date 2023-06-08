986a191fb770d135079f990fab14c092e5f50eb1
Matthew W <fatboychummy@gmail.com>
2023-06-07 23:13:25 -0600

    Change fatlibs to a general purpose installer.

--- install.lua
+++ install.lua
@@ -1,8 +1,17 @@
---- Simple library that can be used to install libraries.
+--- Simple program to be used as an installer script. Copy to repos and insert what is needed.
+
+local to_get = {
+  "extern:filename.lua:https://url.url/", -- if you need an external url
+  "paste:filename.lua:pastecode", -- to download from pastebin
+  "filename.lua:filename_on_repo.lua" -- to download from the Fatboychummy-CC/Libraries repository.
+}
+local program_name = ""
+
+-- #########################################
 
 local RAW_URL = "https://raw.githubusercontent.com/Fatboychummy-CC/Libraries/main/"
-
 local PASTE_URL = "https://pastebin.com/raw/"
+local p_dir = ... or fs.getDir(shell.getRunningProgram())
 
 local function download_file(url, filename)
   print("Downloading", filename)
@@ -11,7 +20,7 @@ local function download_file(url, filename)
     local data = h_handle.readAll()
     h_handle.close()
 
-    local f_handle, err2 = fs.open(filename, 'w') --[[@as WriteHandle]]
+    local f_handle, err2 = fs.open(fs.combine(p_dir, filename), 'w') --[[@as WriteHandle]]
     if f_handle then
       f_handle.write(data)
       f_handle.close()
@@ -25,7 +34,7 @@ local function download_file(url, filename)
   error(("Failed to connect: %s"):format(err), 0)
 end
 
-return function(...)
+local function get(...)
   local remotes = table.pack(...)
 
   for i = 1, remotes.n do
@@ -49,4 +58,28 @@ return function(...)
       error(("Could not determine information for '%s'"):format(remote), 0)
     end
   end
+end
+
+-- Installation is from the installer's directory.
+if p_dir:match("^rom") then
+  error("Attempting to install to the ROM. Please rerun but add arguments for install location (or run the installer script in the folder you wish to install to).", 0)
+end
+
+write(("Going to install to:\n  %s\n\nIs this where you want it to be installed? (y/n): "):format(fs.combine(p_dir, "*")))
+
+local key
+repeat
+  local _, _key = os.pullEvent("key")
+  key = _key
+until key == keys.y or key == keys.n
+
+if key == keys.y then
+  print("y")
+  sleep()
+  print(("Installing %s."):format(program_name))
+  get(table.unpack(to_get))
+else
+  print("n")
+  sleep()
+  error("Installation cancelled.", 0)
 end
