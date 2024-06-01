ae0ece13af5488ce55b05f2d5d913801984fa2de
Matthew W <fatboychummy@gmail.com>
2024-06-01 02:33:12 -0600

    Empty tests for the pine API

---
+++ tests/pine_api/auth/profile.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: undefined-global We will be using a bunch of globals here defined in another location.
+
+local pine_api = require "pine_api" --[[@as pine_store-base]]
+
+suite.suite "Pine API - Authed Profile Endpoint"
+  "PASS" (function()
+    PASS()
+  end)
