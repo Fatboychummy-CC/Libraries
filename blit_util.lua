d8b9d11e2633efc113aa0c33a747c52bcc08befa
Matthew W <fatboychummy@gmail.com>
2024-08-26 16:59:56 -0600

    add `solid_colors` and `combine` methods

--- blit_util.lua
+++ blit_util.lua
@@ -7,6 +7,94 @@ local expect = require "cc.expect".expect
 ---@class blit_util
 local blit_util = {}
 
+--- Give an entire string a solid blit background and text color.
+---@param message string The message to blit.
+---@param text_color string|integer The text color to use.
+---@param bg_color string|integer The background color to use.
+---@return string text The blit text (the same as inputted).
+---@return string text_color The blit text color.
+---@return string bg_color The blit background color.
+function blit_util.solid_colors(message, text_color, bg_color)
+  expect(1, message, "string")
+  expect(2, text_color, "string", "number")
+  expect(3, bg_color, "string", "number")
+
+  if type(text_color) == "number" then
+    text_color = colors.toBlit(text_color)
+  end
+  if type(bg_color) == "number" then
+    bg_color = colors.toBlit(bg_color)
+  end
+
+  text_color = text_color:rep(#message)
+  bg_color = bg_color:rep(#message)
+
+  return message, text_color, bg_color
+end
+
+--- Combine values into valid blit strings, in the format of
+--- 
+--- --> `a1 .. a2 .. a3, t1 .. t2 .. t3, b1 .. b2 .. b3`.
+--- 
+--- The input values are expected to be in the order
+--- 
+--- --> `a1, t1, b1, a2, t2, b2, a3, t3, b3`.
+--- 
+--- ```lua
+--- local text, text_color, bg_color = blit_util.combine(
+---   -- Normal usage: black text, white background
+---   "Hello ", "ffffff", "000000", 
+--- 
+---   -- Table overload, allows daisychaining of blit_util functions
+---   { blit_util.solid_background("world!", colors.white, colors.black) } 
+--- )
+--- 
+--- term.blit(text, text_color, bg_color)
+--- ```
+---@param ... string|table The values to combine. Tables will be pulled apart to allow for multiple values if calling functions.
+---@return string text The combined text values.
+---@return string text_color The combined text color values.
+---@return string bg_color The combined background color values.
+function blit_util.combine(...)
+  local text = ""
+  local text_color = ""
+  local bg_color = ""
+
+  local args = table.pack(...)
+
+  local fixed_args = {}
+
+  -- Rip subtables out and insert them as if they were arguments.
+  for i = 1, args.n do
+    local arg = args[i]
+    if type(arg) == "table" then
+      for j = 1, #arg do
+        table.insert(fixed_args, arg[j])
+      end
+    else
+      table.insert(fixed_args, arg)
+    end
+  end
+
+  for i = 1, #fixed_args, 3 do
+    local s1, s2, s3 = fixed_args[i], fixed_args[i + 1], fixed_args[i + 2]
+    expect(i, s1, "string")
+    expect(i + 1, s2, "string")
+    expect(i + 2, s3, "string")
+
+    if #s1 ~= #s2 or #s1 ~= #s3 then
+      error(("Bad arguments %d, %d, %d: Must be same length (got %d, %d, %d)"):format(i, i + 1, i + 2, #s1, #s2, #s3), 2)
+    end
+
+    text = text .. s1
+    text_color = text_color .. s2
+    bg_color = bg_color .. s3
+  end
+
+  return text, text_color, bg_color
+end
+
+--- Writes text to the screen in `blit` format, with word wrapping.
 ---@param body string The text to print.
 ---@param fg_color string The colors to use for the text.
 ---@param bg_color string The colors to use for the background.
